2c17363e39f1a81b32d57276d71ed6849ca80611
jamigeo <ja.nboehm75@gmail.com>
2025-01-25 03:34:38 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,13 +7,19 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install unzip -y
 
 # download generator.sh
-wget https://raw.githubusercontent.com/username/passwort-generator-installer/main/generator.sh
+wget https://github.com/jamigeo/passwordGenerator.git/generator.sh
 
 # make the script generator.sh executable
 chmod +x generator.sh
 
 # move generator.sh to /usr/local/bin/
-sudo mv generator.sh /usr/local/bin/passwort-generator
+sudo mv generator.sh ~/.local/share/applications/generator.sh
+
+# download the icon
+wget https://github.com/jamigeo/passwordGenerator.git/password-IconWhite.svg
+
+# move the icon to /usr/share/icons/
+sudo mv password-IconWhite.svg ~/.local/share/applications/password-IconWhite.svg
 
 # setup the desktop
 echo "[Desktop Entry]
@@ -21,9 +27,8 @@ Version=1.0
 Type=Application
 Name=Passwort Generator
 Comment=Generiert sichere Passwörter
-Exec=/usr/local/bin/passwort-generator
-Icon=utilities-terminal
+Exec=~/.local/share/applications/generator.sh
+Icon=/.local/share/applications/password-IconWhite.svg
 Path=
 Terminal=true
-StartupNotify=false" > ~/.local/share/applications/passwort-generator.desktop
-
+StartupNotify=false" > ~/.local/share/applications/password-generator.desktop
